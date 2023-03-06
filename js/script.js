5dfbb774d6a6e6dd78f1e348d1af5776c28c7236
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-03-05 20:43:23 -1000

    added a clear button to clear grid

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('#container');
+const clear = document.querySelector('#clear');
+
 
 function grid(num) {
     for(let n = 0; n < num; n++) {
@@ -8,10 +10,12 @@ function grid(num) {
         squares.addEventListener('mouseover', () => {
             squares.style.cssText = 'background-color: black; border 1px solid black; height: 50px; width: 50px';
         })
+        clear.addEventListener('click', () => {
+            squares.style.cssText = 'border: 1px solid black; height: 50px; width: 50px';
+        })
     }
 }
 
 
-
-
 grid(256);
+
